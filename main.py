5c8a2662a94410099a1f8ec0b694c89d43b4b612
Colin <43700502+YuehLinChung@users.noreply.github.com>
2022-07-31 01:43:57 +0800

    Update arguments

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ parser.add_argument("--seed", default=-1, type=int, help="Seed for Numpy and PyT
 parser.add_argument("--epochs", default=200, type=int, help="Total number of epochs")
 parser.add_argument("--dataset", default="cifar100", help="Dataset: cifar10|100, tiny, stl10")
 parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet32', help="Backbone: resnet|32|56|18|34|50")
+parser.add_argument('--mlp-dim', default=256, type=int, help='hidden dimension of MLP head (default: 256)')
+parser.add_argument('--out-dim', default=128, type=int, help='projection dimension (default: 128)')
 parser.add_argument('-b', '--batch-size', default=64, type=int,
                     metavar='N',
                     help='mini-batch size (default: 64), this is the total.')
@@ -30,6 +32,10 @@ parser.add_argument('--lr', '--learning-rate', default=0.01, type=float,
 parser.add_argument("--checkpoint", default="", help="location of a checkpoint file, used to resume training")
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',help='number of data loading workers (default: 4)')
 parser.add_argument("--gpu", default="0", type=str, help="GPU id in case of multiple GPUs")
+parser.add_argument('--warmup-epoch', default=20, type=int, help='number of warm-up epochs to only train with NT-Xent loss')
+parser.add_argument('-t', '--temperature', default=0.1, type=float, help='temperature in NT-Xent')
+parser.add_argument('--con', default=0.1, type=float, help='mean concentration in across prototypes')
+parser.add_argument('--eps', default=[0.3, 0.5], nargs='*', type=float, help='epsilons for clustering')
 parser.add_argument('--exp-dir', default="lightning_logs", type=str,help='experiment directory (default: lightning_logs)')
 parser.add_argument('--exp-name', default="", type=str,help='experiment name')
 args = parser.parse_args()
@@ -47,7 +53,12 @@ batch_size = args.batch_size
 epochs = args.epochs
 
 arch = args.arch
+mlp_dim = args.mlp_dim
+out_dim = args.out_dim
 seed = args.seed
+temperature = args.temperature
+concentration = args.con
+eps = args.eps
 
 exp_dir = args.exp_dir
 exp_name = args.exp_name if args.exp_name else "DBCLR_%s_%s_bs%d_round%d"%(dataset, arch, batch_size, seed)
@@ -60,9 +71,11 @@ if __name__== "__main__":
     # cifar10_normalization()
     if dataset in ["cifar10", "cifar100"]:
         transform_train = ContrastiveTransformations(n_views=2, crop_size=32, gaussian_blur=False, jitter_strength=0.5)
-    elif dataset in ["stl10", "tiny"]:
+    elif dataset in ["tiny"]:
+        transform_train = ContrastiveTransformations(n_views=2, crop_size=64, gaussian_blur=True, jitter_strength=1)
+    elif dataset in ["stl10"]:
         transform_train = ContrastiveTransformations(n_views=2, crop_size=96, gaussian_blur=True, jitter_strength=1)
-    
+
     if dataset == 'cifar10':
         dataset_train = datasets.CIFAR10(data_folder,transform=transform_train)
     elif dataset == 'cifar100':
@@ -79,18 +92,17 @@ if __name__== "__main__":
     
     #%%
     model = DBCLR(
-        proj_hidden_dim=256,
-        proj_out_dim=128,
-        lr = args.lr,
-        temperature=0.1,
+        proj_hidden_dim=mlp_dim,
+        proj_out_dim=out_dim,
+        lr = args.lr*batch_size/64,
+        temperature=temperature,
         weight_decay=1e-4,
         arch=arch,
         max_epochs=epochs,
-        concentration=0.1,
+        concentration=concentration,
         con_estimation = True,
-        warmup_epoch=0,
-        eps=[0.3,0.5],
-        # eps=[10,20,30],
+        warmup_epoch=args.warmup_epoch,
+        eps=eps,
         use_mlp = True
         )
 # model = DBCLR.load_from_checkpoint('models/ep100_DBCLR03_res50_stl10_bs256.ckpt')
@@ -100,5 +112,5 @@ if __name__== "__main__":
     model_saver = CheckpointOnEpochs([200], "ep{epoch}_%s.ckpt"%(exp_name))
     
     # limit_train_batches=0.1
-    trainer = pl.Trainer(max_epochs=epochs, precision=16, gpus=1, callbacks=[bar, model_saver],logger=logger)
+    trainer = pl.Trainer(max_epochs=epochs, precision=16, gpus=1, callbacks=[bar, model_saver],logger=logger, deterministic=True)
     trainer.fit(model, train_dataloaders=loader_train,ckpt_path=checkpoint)
